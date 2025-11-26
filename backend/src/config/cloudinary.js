0e53c5ce692acb917d30046362ee31a81968a204
MANASA-REDDY04 <kandadimanasareddy12@gmail.com>
2025-11-26 15:33:12 +0530

    updated urls

--- backend/src/config/cloudinary.js
+++ backend/src/config/cloudinary.js
@@ -1,12 +1,6 @@
 import dotenv from "dotenv";
 dotenv.config({ path: "./.env" }); 
 import { v2 as cloudinary } from "cloudinary";
-console.log("Cloudinary FILE LOADED at:", new Date().toISOString());
-console.log("Cloudinary ENV:", {
-  name: process.env.CLOUDINARY_CLOUD_NAME,
-  key: process.env.CLOUDINARY_API_KEY,
-  secret: process.env.CLOUDINARY_API_SECRET,
-});
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
